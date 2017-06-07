96258e4bb6f0c8a97eeb110a1c19da8e4c207d5a
Alberto Nicoletti <albyx.n@gmail.com>
2017-06-07 15:41:22 +0200

    minus code gen

--- src/ast/MinusNode.java
+++ src/ast/MinusNode.java
@@ -29,8 +29,9 @@ public class MinusNode implements Node {
 
     @Override
     public String codeGeneration() {
-        // TODO: implement
-        return null;
+        return left.codeGeneration()
+                + right.codeGeneration()
+                + "sub\n";
     }
 
     @Override
