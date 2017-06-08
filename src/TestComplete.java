978611cfddb9115219269aa116528ea87f11a4ef
Alberto Nicoletti <albyx.n@gmail.com>
2017-06-08 11:59:03 +0200

    Indentation fixes

--- src/TestComplete.java
+++ src/TestComplete.java
@@ -18,10 +18,10 @@ import ast.Node;
 
 public class TestComplete {
     public static void main(String[] args) throws Exception {
-      
+
         String filename = "input.fool";
 
-		CharStream input = CharStreams.fromFileName(filename);
+        CharStream input = CharStreams.fromFileName(filename);
         FOOLLexer lexer = new FOOLLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         FOOLParser parser = new FOOLParser(tokens);
@@ -29,53 +29,53 @@ public class TestComplete {
         Node ast = visitor.visit(parser.prog()); //generazione AST
 
         //SIMPLISTIC BUT WRONG CHECK OF THE LEXER ERRORS
-        if (lexer.lexicalErrors > 0){
-        	System.out.println("The program was not in the right format. Exiting the compilation process now");
+        if (lexer.lexicalErrors > 0) {
+            System.out.println("The program was not in the right format. Exiting the compilation process now");
         } else {
 
-	        Environment env = new Environment();
-	        ArrayList<SemanticError> err = ast.checkSemantics(env);
-	        
-	        if (err.size() > 0) {
-	        	System.out.println("You had: " + err.size() + " errors:");
-	        	for (SemanticError e : err) {
-					System.out.println("\t" + e);
-				}
-	        } else {
-		        System.out.println("Visualizing AST...");
-		        System.out.println(ast.toPrint(""));
+            Environment env = new Environment();
+            ArrayList<SemanticError> err = ast.checkSemantics(env);
+
+            if (err.size() > 0) {
+                System.out.println("You had: " + err.size() + " errors:");
+                for (SemanticError e : err) {
+                    System.out.println("\t" + e);
+                }
+            } else {
+                System.out.println("Visualizing AST...");
+                System.out.println(ast.toPrint(""));
 
 
-		        Node type = ast.typeCheck(); //type-checking bottom-up 
-		        System.out.println(type.toPrint("Type checking ok! Type of the program is: "));
+                Node type = ast.typeCheck(); //type-checking bottom-up
+                System.out.println(type.toPrint("Type checking ok! Type of the program is: "));
 
-		        // CODE GENERATION  input.fool.asm
-		        String code=ast.codeGeneration(); 
-		        BufferedWriter out = new BufferedWriter(new FileWriter(filename + ".asm"));
-		        out.write(code);
-		        out.close(); 
-		        System.out.println("Code generated! Assembling and running generated code.");
-		        
-		        CharStream inputASM = CharStreams.fromFileName(filename + ".asm");
-		        SVMLexer lexerASM = new SVMLexer(inputASM);
-		        CommonTokenStream tokensASM = new CommonTokenStream(lexerASM);
-		        SVMParser parserASM = new SVMParser(tokensASM);
-		        
-		        parserASM.assembly();
-		        
-		        System.out.println(
-		                "You had: " + lexerASM.lexicalErrors + " lexical errors and "
-                        + parserASM.getNumberOfSyntaxErrors() + " syntax errors."
+                // CODE GENERATION  input.fool.asm
+                String code = ast.codeGeneration();
+                BufferedWriter out = new BufferedWriter(new FileWriter(filename + ".asm"));
+                out.write(code);
+                out.close();
+                System.out.println("Code generated! Assembling and running generated code.");
+
+                CharStream inputASM = CharStreams.fromFileName(filename + ".asm");
+                SVMLexer lexerASM = new SVMLexer(inputASM);
+                CommonTokenStream tokensASM = new CommonTokenStream(lexerASM);
+                SVMParser parserASM = new SVMParser(tokensASM);
+
+                parserASM.assembly();
+
+                System.out.println(
+                        "You had: " + lexerASM.lexicalErrors + " lexical errors and "
+                                + parserASM.getNumberOfSyntaxErrors() + " syntax errors."
                 );
 
-		        if (lexerASM.lexicalErrors>0 || parserASM.getNumberOfSyntaxErrors()>0) {
-					System.exit(1);
-				}
-		
-		        System.out.println("Starting Virtual Machine...");
-		        ExecuteVM vm = new ExecuteVM(parserASM.code);
-		        vm.cpu();
-	        }
+                if (lexerASM.lexicalErrors > 0 || parserASM.getNumberOfSyntaxErrors() > 0) {
+                    System.exit(1);
+                }
+
+                System.out.println("Starting Virtual Machine...");
+                ExecuteVM vm = new ExecuteVM(parserASM.code);
+                vm.cpu();
+            }
         }
 
     }
--- src/ast/ArrowTypeNode.java
+++ src/ast/ArrowTypeNode.java
@@ -1,4 +1,5 @@
 package ast;
+
 import java.util.ArrayList;
 
 import util.Environment;
@@ -6,42 +7,42 @@ import util.SemanticError;
 
 public class ArrowTypeNode implements Node {
 
-  private ArrayList<Node> parlist; 
-  private Node ret;
-  
-  public ArrowTypeNode (ArrayList<Node> p, Node r) {
-    parlist=p;
-    ret=r;
-  }
-    
-  public String toPrint(String s) { //
-	String parlstr="";
-    for (Node par:parlist)
-      parlstr+=par.toPrint(s+"  ");
-	return s+"ArrowType\n" + parlstr + ret.toPrint(s+"  ->") ; 
-  }
-  
-  public Node getRet () { //
-    return ret;
-  }
-  
-  public ArrayList<Node> getParList () { //
-    return parlist;
-  }
+    private ArrayList<Node> parlist;
+    private Node ret;
 
-  @Override
-	public ArrayList<SemanticError> checkSemantics(Environment env) {
-		return new ArrayList<SemanticError>();
-	}
-  
-  //non utilizzato
-  public Node typeCheck () {
-    return null;
-  }
+    public ArrowTypeNode(ArrayList<Node> p, Node r) {
+        parlist = p;
+        ret = r;
+    }
 
-  //non utilizzato
-  public String codeGeneration() {
-		return "";
-  }
+    public String toPrint(String s) { //
+        String parlstr = "";
+        for (Node par : parlist)
+            parlstr += par.toPrint(s + "  ");
+        return s + "ArrowType\n" + parlstr + ret.toPrint(s + "  ->");
+    }
+
+    public Node getRet() { //
+        return ret;
+    }
+
+    public ArrayList<Node> getParList() { //
+        return parlist;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+        return new ArrayList<SemanticError>();
+    }
+
+    //non utilizzato
+    public Node typeCheck() {
+        return null;
+    }
+
+    //non utilizzato
+    public String codeGeneration() {
+        return "";
+    }
 
 }  
