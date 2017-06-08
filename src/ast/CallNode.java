90dfb2e3a5792d03d0c80fef78e1b1ecbf454680
Alberto Nicoletti <albyx.n@gmail.com>
2017-06-08 11:58:19 +0200

    Method node, missing codeGeneration yet

--- src/ast/CallNode.java
+++ src/ast/CallNode.java
@@ -1,100 +1,103 @@
 package ast;
-import java.util.ArrayList;
 
+import lib.FOOLlib;
 import util.Environment;
 import util.SemanticError;
-import lib.FOOLlib;
+
+import java.util.ArrayList;
 
 public class CallNode implements Node {
 
-  private String id;
-  private STentry entry; 
-  private ArrayList<Node> parlist; 
-  private int nestinglevel;
+    private String id;
+    private ArrayList<Node> params = new ArrayList<Node>();
+    private STentry entry;
+    private int nestingLevel;
 
-  
-  public CallNode (String i, STentry e, ArrayList<Node> p, int nl) {
-    id=i;
-    entry=e;
-    parlist = p;
-    nestinglevel=nl;
-  }
-  
-  public CallNode(String text, ArrayList<Node> args) {
-	id=text;
-    parlist = args;
-}
+    public CallNode(String id, ArrayList<Node> params, STentry entry, int nestingLevel) {
+        this.id = id;
+        this.params = params;
+        this.entry = entry;
+        this.nestingLevel = nestingLevel;
+    }
 
-public String toPrint(String s) {  //
-    String parlstr="";
-	for (Node par:parlist)
-	  parlstr+=par.toPrint(s+"  ");		
-	return s+"Call:" + id + " at nestlev " + nestinglevel +"\n" 
-           +entry.toPrint(s+"  ")
-           +parlstr;        
-  }
+    public CallNode(String id, ArrayList<Node> params) {
+        this.id = id;
+        this.params = params;
+    }
 
-@Override
-	public ArrayList<SemanticError> checkSemantics(Environment env) {
-		//create the result
-		ArrayList<SemanticError> res = new ArrayList<SemanticError>();
-		
-		 int j=env.nestingLevel;
-		 STentry tmp=null; 
-		 while (j>=0 && tmp==null)
-		     tmp=(env.symTable.get(j--)).get(id);
-		 if (tmp==null)
-			 res.add(new SemanticError("Id "+id+" not declared"));
-		 
-		 else{
-			 this.entry = tmp;
-			 this.nestinglevel = env.nestingLevel;
-			 
-			 for(Node arg : parlist)
-				 res.addAll(arg.checkSemantics(env));
-		 }
-		 return res;
-	}
-  
-  public Node typeCheck () {  //                           
-	 ArrowTypeNode t=null;
-     if (entry.getType() instanceof ArrowTypeNode) t=(ArrowTypeNode) entry.getType(); 
-     else {
-       System.out.println("Invocation of a non-function "+id);
-       System.exit(0);
-     }
-     ArrayList<Node> p = t.getParList();
-     if ( !(p.size() == parlist.size()) ) {
-       System.out.println("Wrong number of parameters in the invocation of "+id);
-       System.exit(0);
-     } 
-     for (int i=0; i<parlist.size(); i++) 
-       if ( !(FOOLlib.isSubtype( (parlist.get(i)).typeCheck(), p.get(i)) ) ) {
-         System.out.println("Wrong type for "+(i+1)+"-th parameter in the invocation of "+id);
-         System.exit(0);
-       } 
-     return t.getRet();
-  }
-  
-  public String codeGeneration() {
-	    String parCode="";
-	    for (int i=parlist.size()-1; i>=0; i--)
-	    	parCode+=parlist.get(i).codeGeneration();
-	    
-	    String getAR="";
-		  for (int i=0; i<nestinglevel-entry.getNestinglevel(); i++) 
-		    	 getAR+="lw\n";
-	    
-		return "lfp\n"+ //CL
-               parCode+
-               "lfp\n"+getAR+ //setto AL risalendo la catena statica
-               // ora recupero l'indirizzo a cui saltare e lo metto sullo stack
-               "push "+entry.getOffset()+"\n"+ //metto offset sullo stack
-		       "lfp\n"+getAR+ //risalgo la catena statica
-			   "add\n"+ 
-               "lw\n"+ //carico sullo stack il valore all'indirizzo ottenuto
-		       "js\n";
-  }
+    public String toPrint(String indent) {  //
+        String paramsToString = params.stream()
+                .map(param -> param.toPrint(indent + "  "))
+                .reduce("", String::concat);
+        return indent + "Call method: " + id
+                + " at nesting level " + nestingLevel + "\n"
+                + entry.toPrint(indent + "  ")
+                + paramsToString;
+    }
 
-    
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+        //create the result
+        ArrayList<SemanticError> res = new ArrayList<SemanticError>();
+
+        int j = env.nestingLevel;
+        STentry tmp = null;
+        while (j >= 0 && tmp == null)
+            tmp = (env.symTable.get(j--)).get(id);
+        if (tmp == null)
+            res.add(new SemanticError("Id " + id + " not declared"));
+
+        else {
+            this.entry = tmp;
+            this.nestingLevel = env.nestingLevel;
+
+            for (Node arg : params)
+                res.addAll(arg.checkSemantics(env));
+        }
+        return res;
+    }
+
+    public Node typeCheck() {  //
+        ArrowTypeNode t = null;
+        if (entry.getType() instanceof ArrowTypeNode) {
+            t = (ArrowTypeNode) entry.getType();
+        } else {
+            System.out.println("Invocation of a non-function " + id);
+            System.exit(0);
+        }
+
+        ArrayList<Node> p = t.getParList();
+        if (!(p.size() == params.size())) {
+            System.out.println("Wrong number of parameters in the invocation of " + id);
+            System.exit(0);
+        }
+        for (int i = 0; i < params.size(); i++)
+            if (!(FOOLlib.isSubtype((params.get(i)).typeCheck(), p.get(i)))) {
+                System.out.println("Wrong type for " + (i + 1) + "-th parameter in the invocation of " + id);
+                System.exit(0);
+            }
+        return t.getRet();
+    }
+
+    public String codeGeneration() {
+        String parCode = "";
+        for (int i = params.size() - 1; i >= 0; i--)
+            parCode += params.get(i).codeGeneration();
+
+        String getAR = "";
+        for (int i = 0; i < nestingLevel - entry.getNestinglevel(); i++)
+            getAR += "lw\n";
+
+        return "lfp\n" + //CL
+                parCode +
+                "lfp\n" + getAR + //setto AL risalendo la catena statica
+                // ora recupero l'indirizzo a cui saltare e lo metto sullo stack
+                "push " + entry.getOffset() + "\n" + //metto offset sullo stack
+                "lfp\n" + getAR + //risalgo la catena statica
+                "add\n" +
+                "lw\n" + //carico sullo stack il valore all'indirizzo ottenuto
+                "js\n";
+    }
+
+
 }  
--- src/ast/MethodNode.java
+++ src/ast/MethodNode.java
@@ -6,27 +6,48 @@ import util.SemanticError;
 import java.util.ArrayList;
 
 public class MethodNode implements Node {
+
+    private String id;
+    private ArrayList<Node> params = new ArrayList<Node>();
+    private STentry entry;
+    private int nestingLevel;
+
+    public MethodNode(String id, ArrayList<Node> params, STentry entry, int nestingLevel) {
+        this.id = id;
+        this.params = params;
+        this.entry = entry;
+        this.nestingLevel = nestingLevel;
+    }
+
+    public MethodNode(String id, ArrayList<Node> params) {
+        this.id = id;
+        this.params = params;
+    }
+
     @Override
     public String toPrint(String indent) {
-        // TODO: implement
-        return null;
+        String paramsToString = params.stream()
+                .map(param -> param.toPrint(indent + "  "))
+                .reduce("", String::concat);
+        return indent + "Call method: " + id
+                + " at nesting level " + nestingLevel + "\n"
+                + entry.toPrint(indent + "  ")
+                + paramsToString;
     }
 
     @Override
     public Node typeCheck() {
-        // TODO: implement
-        return null;
-    }
-
-    @Override
-    public String codeGeneration() {
-        // TODO: implement
-        return null;
+        return new CallNode(id, params, entry, nestingLevel).typeCheck();
     }
 
     @Override
     public ArrayList<SemanticError> checkSemantics(Environment env) {
-        // TODO: implement
-        return null;
+        return new CallNode(id, params, entry, nestingLevel).checkSemantics(env);
     }
+
+    @Override
+    public String codeGeneration() {
+        return "";
+    }
+
 }
