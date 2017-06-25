ec7802c4b047388358f5c322ed8b61c950f8423d
Devid Farinelli <devid.farinelli@gmail.com>
2017-06-25 20:53:53 +0200

    aggiunta una bozza di reference counter

--- src/vm/HeapMemoryCell.java
+++ src/vm/HeapMemoryCell.java
@@ -10,14 +10,19 @@ class HeapMemoryCell {
      * L'indice di memoria corrispondente a questa cella
      */
     private int index;
+    private int referenceCounter;
 
     HeapMemoryCell(int index, HeapMemoryCell next) {
         this.index = index;
         this.next = next;
+        this.referenceCounter = 0;
     }
 
     int getIndex() {
         return this.index;
     }
 
+    int incReference() { return ++this.referenceCounter; }
+
+    int decReference() { return --this.referenceCounter; }
 }
